1ac34eba21cf11160e439db0139d3c1853486e6d
daniel udoncy <danielcyudoncy@gmail.com>
2024-10-24 06:26:57 +0100

    correcting the splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,13 +1,18 @@
+import 'package:assignmentapp/controllers/splash_controller.dart';
 import 'package:assignmentapp/utils/constants/app_colors.dart';
 import 'package:assignmentapp/utils/constants/app_fonts_family.dart';
 import 'package:assignmentapp/utils/constants/app_sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Initialize the SplashController
+    Get.put(SplashController());
+
     return Scaffold(
       backgroundColor: AppColors.primaryColor,
       body: Center(
@@ -18,6 +23,7 @@ class SplashScreen extends StatelessWidget {
               'assets/png/logo.png',
               height: 188,
               width: 180,
+              semanticLabel: 'App Logo',
             ),
             const SizedBox(height: 20),
             const Text(
